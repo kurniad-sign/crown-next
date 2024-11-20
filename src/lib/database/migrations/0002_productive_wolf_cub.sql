20360154be0f25288642f7d493923e96645fc093
Agung Kurniadi <agungkurniadi99@gmail.com>
2024-11-20 11:18:43 +0700

    chore: change type data table store

---
+++ src/lib/database/migrations/0002_productive_wolf_cub.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "stores" ALTER COLUMN "store_id" DROP NOT NULL;
--- src/lib/database/schemas/stores.ts
+++ src/lib/database/schemas/stores.ts
@@ -1,14 +1,14 @@
 import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
-import { integer, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
 export const stores = pgTable('stores', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity().notNull(),
+  id: uuid('id').primaryKey().default('uuid_generate_v4()'),
   name: varchar('name', { length: 255 }).notNull(),
-  userId: varchar('user_id', { length: 255 }),
+  userId: varchar('user_id', { length: 255 }).notNull(),
   storeId: varchar('store_id', { length: 255 }).unique(),
   storeLogoUrl: varchar('store_logo_url', { length: 255 }),
-  createdAt: timestamp('created_at').defaultNow(),
-  updatedAt: timestamp('updated_at').defaultNow(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
 
 export type StoresDataType = InferSelectModel<typeof stores>
